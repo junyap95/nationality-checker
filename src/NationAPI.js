ac364ca927b42358c114476ca697039affbda1ed
junyap95 <junyap0122@gmail.com>
2024-03-19 19:23:46 +0000

    refactored Nation.js

--- src/NationAPI.js
+++ src/NationAPI.js
@@ -16,44 +16,62 @@ function NationAPI() {
       "https://api.nationalize.io/?name=" + userInput
     );
     let newDataID = await responseID.json();
-
+    console.log(newDataID.country);
     // allow for 2 possibilities of country
-    let countryID1 = newDataID.country[0].country_id;
-    let countryID2 = newDataID.country[1].country_id;
+    let countryIDs = [];
 
-    // because the API has got a typo where 'SQ' is supposed to be 'SG'
-    if (countryID1 === "SQ") {
-      countryID1 = "SG";
-    }
-    if (countryID2 === "SQ") {
-      countryID2 = "SG";
-    }
-
-    // we are using the country ID extracted to fetch country information from this endpoint, including country name
-    let responseName1 = await fetch(
-      "https://restcountries.com/v3.1/alpha?codes=" + countryID1
-    );
-
-    let responseName2 = await fetch(
-      "https://restcountries.com/v3.1/alpha?codes=" + countryID2
-    );
-
-    let newDataName1 = await responseName1.json();
-    let newDataName2 = await responseName2.json();
-
-    try {
-      const countryName1 = JSON.stringify(
-        newDataName1[0].name.common
-      ).replaceAll('"', "");
-
-      const countryName2 = JSON.stringify(
-        newDataName2[0].name.common
-      ).replaceAll('"', "");
-
-      const textString = `${userInput} is from ${countryName1}...\nOR maybe ${countryName2}`;
+    let textString;
+    if (newDataID.country.length === 0) {
+      textString = `We can't locate ${userInput}...`;
       setData(textString);
-    } catch (error) {
-      console.error(error);
+    } else {
+      let responseName1;
+      let responseName2;
+      let newDataName1;
+      let newDataName2;
+
+      for (let i = 0; i < newDataID.country.length; i++) {
+        let countryID = newDataID.country[i].country_id;
+        if (countryID === "SQ") {
+          countryID = "SG";
+        }
+        countryIDs.push(countryID);
+      }
+      responseName1 = await fetch(
+        "https://restcountries.com/v3.1/alpha?codes=" + countryIDs[0]
+      );
+      newDataName1 = await responseName1.json();
+
+      if (countryIDs.length > 1) {
+        responseName2 = await fetch(
+          "https://restcountries.com/v3.1/alpha?codes=" + countryIDs[1]
+        );
+        newDataName2 = await responseName2.json();
+      }
+      // we are using the country ID extracted to fetch country information from this endpoint, including country name
+
+      let countryName1;
+      let countryName2;
+      try {
+        countryName1 = JSON.stringify(newDataName1[0].name.common).replaceAll(
+          '"',
+          ""
+        );
+
+        if (countryIDs.length > 1) {
+          countryName2 = JSON.stringify(newDataName2[0].name.common).replaceAll(
+            '"',
+            ""
+          );
+        }
+        textString =
+          countryIDs.length > 1
+            ? `${userInput} is from ${countryName1}...\nOR maybe ${countryName2}`
+            : `${userInput} is from ${countryName1}`;
+        setData(textString);
+      } catch (error) {
+        console.error(error);
+      }
     }
   }, [userInput]);
 
